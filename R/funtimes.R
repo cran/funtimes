c17110e630e80bec77764cf934be4ea32da6bb56
Vyacheslav Lyubchich <lyubchic@umces.edu>
2019-02-17 21:30:03 +0000

    version 6.0

---
+++ R/funtimes.R
@@ -0,0 +1,7 @@
+#' @importFrom dbscan dbscan
+#' @importFrom Jmisc demean
+#' @importFrom Kendall MannKendall
+#' @importFrom Rdpack reprompt
+#' @importFrom stats ar arima.sim as.formula cov dist embed filter IQR lm median na.omit pnorm qnorm quantile rnorm sd var
+#' @importFrom utils combn
+NULL
